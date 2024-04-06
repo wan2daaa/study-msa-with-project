f067d15bdf0fda4952b60f5a2d975cf9ba2a5dc3
wan2daaa <wan2daaa@gmail.com>
2024-04-06 21:30:16 +0900

    init commit

---
+++ src/main/java/me/wane/studymsawithproject/StudyMsaWithProjectApplication.java
@@ -0,0 +1,13 @@
+package me.wane.studymsawithproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMsaWithProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudyMsaWithProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/wane/studymsawithproject/StudyMsaWithProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.wane.studymsawithproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMsaWithProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
